2ee55208de34bbd2703f2d7d05875d0d88a5ee37
Abhishek Kumar Gupta <ag6557168@gmail.com>
2026-08-14 17:13:12 +0530

    updated project

---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Brojkerage from './Brojkerage'
+import Hero from './Hero'
+
+const PricingPage = () => {
+  return (
+   <>
+   <Brojkerage />
+   <Hero/>
+   </>
+  )
+}
+
+export default PricingPage
